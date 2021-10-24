975d7725de5fc5d57b8433a1a14911eddffd09bd
chuxmykel <ngwobiachukwudi@gmail.com>
2021-10-25 00:43:55 +0100

    write tests for the app controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,8 +1,7 @@
-import { Controller, Request, Post, UseGuards, Get } from '@nestjs/common';
+import { Controller, Request, Post, UseGuards } from '@nestjs/common';
 
 import { AuthService } from './auth/auth.service';
 import { LocalAuthGuard } from './auth/local-auth.guard';
-import { JwtAuthGuard } from './auth/jwt-auth.guard';
 
 @Controller()
 export class AppController {
@@ -11,11 +10,5 @@ export class AppController {
   @Post('auth/login')
   async login(@Request() req) {
     return this.authService.login(req.user);
-  }
-
-  @UseGuards(JwtAuthGuard)
-  @Get('profile')
-  getProfile(@Request() req) {
-    return req.user;
   }
 }
--- src/auth/__fixtures__/index.ts
+++ src/auth/__fixtures__/index.ts
@@ -1,1 +1,4 @@
 export const mockJWT = 'JWToken';
+export const mockLoginResponse: { access_token: string } = {
+  access_token: mockJWT,
+};
